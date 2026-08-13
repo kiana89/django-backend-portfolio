890222e8673fa1fac54362d191b5c5fcffb0ed3e
kianaas <kianaas8089@gmail.com>
2026-08-14 00:13:41 +0330

    Add Faker test data for collaboration requests

--- collaborationrequest/admin.py
+++ collaborationrequest/admin.py
@@ -3,6 +3,6 @@ from .models import CollaborationRequest
 
 @admin.register(CollaborationRequest)
 class CollaborationRequestAdmin(admin.ModelAdmin):
-    list_display = ('person_type', 'full_name', 'company_name', 'email', 'phone', 'subject', 'message', 'create_at', 'update_at', 'is_read',)
+    list_display = ('id', 'person_type', 'full_name', 'company_name', 'email', 'phone', 'subject', 'message', 'create_at', 'update_at', 'is_read',)
     list_filter = ('person_type','is_read','create_at',)
     search_fields = ('full_name','company_name','email','subject',)
